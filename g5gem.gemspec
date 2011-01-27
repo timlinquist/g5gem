4d6c527513cd0efd232512d2b003f335517c6fa3
Mark Turner <mark@amerine.net>
2011-01-26 20:16:45 -0800

    Have it working

--- g5gem.gemspec
+++ g5gem.gemspec
@@ -19,4 +19,5 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
   s.add_dependency('thor', '0.14.6')
+  s.add_dependency('net-scp', '1.0.4')
 end
--- lib/g5gem.rb
+++ lib/g5gem.rb
@@ -1,5 +1,16 @@
+require 'net/ssh'
 require 'net/scp'
 
 module G5gem
+  def self.transer_file_to_server(file)
+    Net::SCP.start("util1", "g5search") do |scp|
+      scp.upload(File.expand_path(file), "/shared/gems/gems")
+    end
+  end
 
+  def self.rebuild_index
+    Net::SSH.start("util1", "g5search") do |ssh|
+      ssh.exec "/usr/bin/gem generate_index -d /shared/gems"
+    end
+  end
 end
